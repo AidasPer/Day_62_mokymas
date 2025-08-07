09d1d195f7af9a24543cb861918024c4b6b78927
perai0980@gmail.com <perai0980@gmail.com>
2025-08-07 20:49:32 +0300

    prideti uzduoties reikalavimai

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+# Sukurti naują projektą mokymas
+#
+# Projekto kataloge sukurti katalogą models
+#
+# Kataloge models sukurti failą kursas.py
+#
+# Faile kursas.py sukurti objekto klasę Kursas, kuri turėtų savybes pavadinimas, destytojas, trukme, taip pat metodą destyti(), kuris spausdintų „Vyksta mokymas!“
+#
+# Kataloge models sukurti antrą failą python_kursas.py
+#
+# Faile python_kursas.py sukurti objekto klasę PythonKursas, kuri paveldėtų viską iš klasės Kursas, bei perrašytų metodą destyti() taip, kad jis spausdintų „Vyksta programavimas!“
+#
+# Pagrindiniame projekto kataloge sukurti failą main.py
+#
+# Faile main.py importuoti Kursas modulį (failą)
+#
+# Faile main.py importuoti PythonKursas modulį (failą)
+#
+# Faile main.py inicijuoti Kursas objektą su norimomis savybėmis
+#
+# Faile main.py inicijuoti PythonKursas objektą su norimomis savybėmis
+#
+# Paleisti abiejų objektų metodą destyti()
+
+
 from models.kursas import Kursas
 from models.python_kursas import PythonKursas
 
